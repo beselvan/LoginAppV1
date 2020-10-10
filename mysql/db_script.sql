c0a09ce7e2eb86bfb9b7bbbc59394bf912c5fd77
beselvan <43469566+beselvan@users.noreply.github.com>
2020-10-10 11:35:09 +0530

    Update db_script.sql

--- mysql/db_script.sql
+++ mysql/db_script.sql
@@ -2,7 +2,7 @@ create database demo;
 use demo;
 
 create table user (first_name varchar(30), last_name varchar(30), email varchar(30), username varchar(30), password varchar(30), regdate date, registeredIP varchar(20));
-create table logindetails (username VARCHAR(30), logindate DATE, logintime TIME,logoutdate DATE, logouttime TIME, session VARCHAR(30),);
+create table logindetails (username VARCHAR(30), logindate DATE, logintime TIME,logoutdate DATE, logouttime TIME, session VARCHAR(30));
 
 insert into user(first_name, last_name, email, username, password, regdate,registeredIP) values ('test','test','test@test.com','test','test', CURDATE(),'10.10.10.10');
 
